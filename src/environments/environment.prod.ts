65afdec5a66fb962bdf2f2402211bf834a8f86d7
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-07-27 01:38:56 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,5 @@ export const environment = {
   newsApi: 'https://api.first.org/data/v1/news',
   countryCode: 'https://countrycode.org/api/countryCode/countryMenu',
   countryCodeWithIndex: 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes',
-  firebaseConfig: {
-    apiKey: '${process.env.firebaseConfig}'
-},
+  firebaseConfig: '${{ secrets.FIREBASECONFIG }}'
 };
