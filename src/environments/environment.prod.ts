84f17ddd00420137daaf2216ea448917b4c2ff10
Anusmrity Dolai <anusmritydolai1995@gmail.com>
2021-07-27 00:48:50 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
   newsApi: 'https://api.first.org/data/v1/news',
   countryCode: 'https://countrycode.org/api/countryCode/countryMenu',
-  countryCodeWithIndex: 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes'
+  countryCodeWithIndex: 'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code;callingCodes',
+  firebaseConfig: {
+    apiKey: '${process.env.firebaseConfig}'
+},
 };
